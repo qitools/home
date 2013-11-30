25090d803c09f915fcc3c02ae3df2d8462a98069
Robert Badgett <rbadgett@kumc.edu>
2013-11-30 10:41:02 -0600

    New files

---
+++ R/qitools.R
@@ -0,0 +1,16 @@
+#' qitools
+#' 
+#' This collection of functions implements selected functions from qcc.
+#' 
+#' @author Robert Badgett
+#' @import ggplot2 httr RJSONIO reshape2
+#' @export
+qitools <- function () {
+	
+	myplot <- plot.new()
+	text(0,0,getwd())
+	
+	#don't return anything
+	print(myplot)  
+	invisible();
+}
